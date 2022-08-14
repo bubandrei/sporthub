3c3d7d57d2997617136b9bdf9ca6aa725d7d5f0e
a.bubelev <bubeleu@gmail.com>
2022-08-14 19:24:41 +0300

    content with problems adaptive

--- src/component/Content/content.jsx
+++ src/component/Content/content.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styleContent from "./content.module.css";
 import foto1 from "../../assets/image/2.png";
 import foto2 from "../../assets/image/3.png";
+import foto4 from "../../assets/image/4.png";
 
 const Content = () => {
   return (
@@ -49,22 +50,25 @@ const Content = () => {
       <section className={styleContent._container}>
         <div className={styleContent.firstArticle}>
           <p className={styleContent.header}></p>
-          <div className={styleContent.contentFoto}>
-            <img src={foto1} alt="" className={styleContent.articleFoto} />
+          <div className={styleContent.contentFotoBalck}>
+            <img src={foto4} alt="" className={styleContent.articleFoto} />
           </div>
+          <div className={styleContent.blackBack}>
           <p className={styleContent.dateContent}>
-            <span className={styleContent.date}>09.07.19 |</span>
-            <span className={styleContent.dateFill}>#Профессиональный_спорт</span>
+            <span className={styleContent.date2}>09.07.19 |</span>
+            <span className={styleContent.dateFill2}>#Профессиональный_спорт</span>
           </p>
-          <p className={styleContent.titleContent}>
+          <p className={styleContent.titleContent2}>
             Японцы лидируют в медальном зачете летней универсиады, белорусы –
             25-е. Почему так?
           </p>
-          <p className={styleContent.fillContent}>
+          <p className={styleContent.fillContent2}>
             И если для «горняков», которые неделей ранее разгромно уступили в
             Турине тамошним «быкам» 0:5, визит «Торино» в Солигорск —
             формальность, то от «жёлто-синих»...
           </p>
+          </div>
+
         </div>
         <div className={styleContent.firstArticle}>
           <p className={styleContent.header}></p>
