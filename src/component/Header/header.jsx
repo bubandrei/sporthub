0bd827dfc69e653f5bf58568ad96e6301dd97712
a.bubelev <bubeleu@gmail.com>
2022-08-14 16:17:31 +0300

    version 3.0

--- src/component/Header/header.jsx
+++ src/component/Header/header.jsx
@@ -1,64 +1,44 @@
 import React from "react";
 import style from "./header.module.css";
-import image1 from "../../assets/image/1.png";
+import logo from "../../assets/image/logo.png";
 
 const Header = () => {
   return (
-    <div className={style._container}>
-      <header>
-        <div className={`${style._container} ${style.wrap}`}>
-          <a href="#">
-            <span className={style.firstLetter}>sport</span>
-            <span className={style.secondLetter}>hub</span>
-          </a>
-          <nav className={style.headerMenu}>
-            <ul className={style.menuClass}>
-              <li className={style.menuItem}>
-                <a href="#" className={`${style.menuLink} ${style.burger}`}>
-                  Review
-                </a>
-              </li>
-              <li className={style.menuItem}>
-                <a href="#" className={`${style.menuLink} ${style.burger}`}>
-                  Экстрим
-                </a>
-              </li>
-              <li className={style.menuItem}>
-                <a href="#" className={`${style.menuLink} ${style.burger}`}>
-                  Легенды
-                </a>
-              </li>
-              <li className={style.menuItem}>
-                <a href="#" className={`${style.menuLink} ${style.burger}`}>
-                  зож и туризм
-                </a>
-              </li>
-              <li className={style.menuItem}>
-                <a href="#" className={`${style.menuLink} ${style.burger}`}>
-                  Позірк
-                </a>
-              </li>
-              <li className={style.menuItem}>
-                <a href="#" className={`${style.menuLink} ${style.burger}`}>
-                  Треш
-                </a>
-              </li>
-              <li className={style.menuItem}>
-                <a href="#" className={`${style.menuLink}`}>
-                  #######
-                </a>
-              </li>
-            </ul>
-          </nav>
+    <div>
+      <header className={style.header}>
+        <div className={style._container}>
+          <div className={style.header_row}>
+            <div className={style.headerLogo}>
+              <img src={logo} alt="LOGO" />
+            </div>
+            <nav className={style.headerMenu}>
+              <ul className={style.headerNav}>
+                <li>
+                  <a href="#">Review</a>
+                </li>
+                <li>
+                  <a href="#">Экстрим</a>
+                </li>
+                <li>
+                  <a href="#">Легенды</a>
+                </li>
+                <li>
+                  <a href="#">зож и туризм</a>
+                </li>
+                <li>
+                  <a href="#">Позірк</a>
+                </li>
+                <li>
+                  <a href="#">Треш</a>
+                </li>
+                <li>
+                  <a href="#">#######</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </header>
-      <div className={style.content}>
-        <div className={style.topContent}>#Экстрим</div>
-        <div className={style.bottomContent}>Юрий Поклад: «Динамо» как бы начинает работать заново</div>
-      </div>
-      {/* <div className={style.blockImg}>
-        <img src={image1}/>
-      </div> */}
     </div>
   );
 };
