e980c833f05042a0804e62b2fa2d3ccf361d3005
a.bubelev <bubeleu@gmail.com>
2022-08-14 20:22:39 +0300

    second section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Content from './component/Content/content';
+import Content2 from './component/Content2/content2';
 import Header from './component/Header/header';
 import News from './component/News/news';
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Content />
       <News/>
+      <Content2/>
     </>
 
   );
---
+++ src/component/Content2/content2.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styleContent from "./content2.module.css";
+import foto2 from "../../assets/image/3.png";
+import foto4 from "../../assets/image/4.png";
+
+const Content2 = () => {
+  return (
+    <>
+      <section className={styleContent._container}>
+        <div className={styleContent.firstArticle}>
+          <p className={styleContent.header}></p>
+          <div className={styleContent.contentFoto}>
+            <img src={foto2} alt="" className={styleContent.articleFoto} />
+          </div>
+          <p className={styleContent.dateContent}>
+            <span className={styleContent.date}>Сегодня |</span>
+            <span className={styleContent.dateFill}>#болельщикам</span>
+          </p>
+          <p className={styleContent.titleContent}>
+            Японцы лидируют в медальном зачете летней универсиады, белорусы –
+            25-е. Почему так?
+          </p>
+          <p className={styleContent.fillContent}>
+            И если для «горняков», которые неделей ранее разгромно уступили в
+            Турине тамошним «быкам» 0:5, визит «Торино» в Солигорск —
+            формальность, то от «жёлто-синих»...
+          </p>
+        </div>
+        <div className={styleContent.firstArticle}>
+          <p className={styleContent.header}></p>
+          <div className={styleContent.contentFotoBalck}>
+            <img src={foto4} alt="" className={styleContent.articleFoto} />
+          </div>
+          <div className={styleContent.blackBack}>
+            <p className={styleContent.dateContent}>
+              <span className={styleContent.date2}>09.07.19 |</span>
+              <span className={styleContent.dateFill2}>
+                #Профессиональный_спорт
+              </span>
+            </p>
+            <p className={styleContent.titleContent2}>
+              Японцы лидируют в медальном зачете летней универсиады, белорусы –
+              25-е. Почему так?
+            </p>
+            <p className={styleContent.fillContent2}>
+              И если для «горняков», которые неделей ранее разгромно уступили в
+              Турине тамошним «быкам» 0:5, визит «Торино» в Солигорск —
+              формальность, то от «жёлто-синих»...
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Content2;
